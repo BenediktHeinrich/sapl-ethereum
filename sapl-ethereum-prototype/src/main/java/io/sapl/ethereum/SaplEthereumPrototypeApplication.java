5864d67361f678736e5c225124569cb8735af7d8
Benedikt Heinrich <benedikt.heinrich@web.de>
2019-04-15 13:38:35 +0200

    Created SAPL-Ethereum-Prototype Application

---
+++ sapl-ethereum-prototype/src/main/java/io/sapl/ethereum/SaplEthereumPrototypeApplication.java
@@ -0,0 +1,13 @@
+package io.sapl.ethereum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaplEthereumPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaplEthereumPrototypeApplication.class, args);
+	}
+
+}

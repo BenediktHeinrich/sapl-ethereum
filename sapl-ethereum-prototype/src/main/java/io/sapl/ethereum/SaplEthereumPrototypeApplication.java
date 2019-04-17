1f8d498b1d56f04f171f59aeb00ad3fcb2c1e391
Benedikt Heinrich <benedikt.heinrich@web.de>
2019-04-17 15:27:24 +0200

    added web3j dependency

--- sapl-ethereum-prototype/src/main/java/io/sapl/ethereum/SaplEthereumPrototypeApplication.java
+++ sapl-ethereum-prototype/src/main/java/io/sapl/ethereum/SaplEthereumPrototypeApplication.java
@@ -2,12 +2,19 @@ package io.sapl.ethereum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class SaplEthereumPrototypeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SaplEthereumPrototypeApplication.class, args);
+	}
+	
+	@EventListener(ApplicationReadyEvent.class)
+	public void ethereumSetup() {
+	    
 	}
 
 }

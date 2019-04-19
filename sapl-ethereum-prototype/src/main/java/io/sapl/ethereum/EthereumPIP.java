8762a7fb3602f99aecad8323f2cf21a6ee2fc0a6
Benedikt Heinrich <benedikt.heinrich@web.de>
2019-04-19 18:29:37 +0200

    added sapl policies

--- sapl-ethereum-prototype/src/main/java/io/sapl/ethereum/EthereumPIP.java
+++ sapl-ethereum-prototype/src/main/java/io/sapl/ethereum/EthereumPIP.java
@@ -6,13 +6,16 @@ import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.gas.DefaultGasProvider;
 
+import io.sapl.api.pip.Attribute;
 import io.sapl.api.pip.PolicyInformationPoint;
 import io.sapl.ethereum.contracts.Authorization;
 
 @PolicyInformationPoint(name="ethereum", description="checks if  user is authorized in ethereum contract")
 public class EthereumPIP {
 	
+	@Attribute
 	public boolean authorized(String user) {
+	System.out.println("Entered authorized...");
 	Web3j web3j = Web3j.build(new HttpService());
 	try {
 		Credentials credentials = WalletUtils.loadCredentials("", "/home/bene/ethereum-testnet/ptn/keystore/UTC--2019-04-17T21-39-40.596498485Z--2678c7e529d61f14f7711053be92d0a923cda8d2");
--- sapl-ethereum-prototype/src/main/java/io/sapl/ethereum/SaplEthereumPrototypeApplication.java
+++ sapl-ethereum-prototype/src/main/java/io/sapl/ethereum/SaplEthereumPrototypeApplication.java
@@ -11,10 +11,17 @@ import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.gas.DefaultGasProvider;
 
+import io.sapl.api.pdp.PolicyDecisionPoint;
+import io.sapl.api.pdp.Response;
+import io.sapl.pdp.embedded.EmbeddedPolicyDecisionPoint;
 import io.sapl.ethereum.contracts.Authorization;
 
 @SpringBootApplication
 public class SaplEthereumPrototypeApplication {
+	
+	public final String USER1_WALLET = "/home/bene/ethereum-testnet/ptn/keystore/UTC--2019-04-17T21-39-40.596498485Z--2678c7e529d61f14f7711053be92d0a923cda8d2";
+	public final String USER1 = "0x2678c7e529d61f14f7711053be92d0a923cda8d2";
+	public final String USER2 = "0x91b6eac43acf5fc115fb30bf8ecc348d1c8d474b";
 
 	public static void main(String[] args) {
 		SpringApplication.run(SaplEthereumPrototypeApplication.class, args);
@@ -24,24 +31,29 @@ public class SaplEthereumPrototypeApplication {
 	public void ethereumSetup() {
 	    Web3j web3 = Web3j.build(new HttpService());
 	    try {
-			Credentials credentials = WalletUtils.loadCredentials("", "/home/bene/ethereum-testnet/ptn/keystore/UTC--2019-04-17T21-39-40.596498485Z--2678c7e529d61f14f7711053be92d0a923cda8d2");
-
+			Credentials credentials = WalletUtils.loadCredentials("", USER1_WALLET);
 			Authorization authContract = Authorization.deploy(web3, credentials, new DefaultGasProvider()).send();
 			
 			System.out.println("Authorization contract deployed under address: \n" + authContract.getContractAddress());
 			
 			System.out.println("User 1 is authorized: " + 
-						authContract.isAuthorized("0x2678c7e529d61f14f7711053be92d0a923cda8d2").send());
+						authContract.isAuthorized(USER1).send());
 			System.out.println("User 2 is authorized: " + 
-						authContract.isAuthorized("0x91b6eac43acf5fc115fb30bf8ecc348d1c8d474b").send());
+						authContract.isAuthorized(USER2).send());
 			
-			authContract.authorize("0x91b6eac43acf5fc115fb30bf8ecc348d1c8d474b").send();
+			authContract.authorize(USER2).send();
 			
 			System.out.println("User 1 is authorized: " + 
-					authContract.isAuthorized("0x2678c7e529d61f14f7711053be92d0a923cda8d2").send());
+					authContract.isAuthorized(USER1).send());
 			System.out.println("User 2 is authorized: " + 
-					authContract.isAuthorized("0x91b6eac43acf5fc115fb30bf8ecc348d1c8d474b").send());
-
+					authContract.isAuthorized(USER2).send());
+			
+			PolicyDecisionPoint pdp = new EmbeddedPolicyDecisionPoint("file:/home/bene/repos-linux/sapl-ethereum/sapl-ethereum-prototype/src/main/resources/policies/");
+			Response user1access = pdp.decide(USER1, "access", "ethereum");
+			Response user2access = pdp.decide(USER2, "access", "ethereum");
+			System.out.println("User1 Response: " + user1access);
+			System.out.println("User2 Response: " + user2access);
+			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
